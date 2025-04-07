03eb1023289dc11705476be7c44150cdfb874cb7
Kyle Reinholdtsen <kyle@reinholdtsen.com>
2025-04-07 03:06:45 -0700

    Add header and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameBoardComponent } from './game-board/game-board.component';
 import { PuzzleDisplayComponent } from "./puzzle-display/puzzle-display.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, GameBoardComponent, PuzzleDisplayComponent],
+  imports: [RouterOutlet, GameBoardComponent, PuzzleDisplayComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
