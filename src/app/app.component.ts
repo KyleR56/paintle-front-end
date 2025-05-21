988fde36020405ea991aebf8db3b939ea73977bd
Kyle Reinholdtsen <kyle@reinholdtsen.com>
2025-05-21 10:25:32 -0700

    Improve mobile support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 
@@ -8,6 +8,18 @@ import { HeaderComponent } from "./components/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'paintle-front-end';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.setViewportHeight();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.setViewportHeight();
+  }
+
+  private setViewportHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
 }
--- src/app/components/paint-pattern-display/paint-pattern-display.component.ts
+++ src/app/components/paint-pattern-display/paint-pattern-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnDestroy, signal, Signal } from '@angular/core';
 import { PaintPatternService } from '../../services/paint-pattern.service';
 
 @Component({
@@ -7,10 +7,32 @@ import { PaintPatternService } from '../../services/paint-pattern.service';
   templateUrl: './paint-pattern-display.component.html',
   styleUrl: './paint-pattern-display.component.css'
 })
-export class PaintPatternDisplayComponent {
+export class PaintPatternDisplayComponent implements AfterViewInit, OnDestroy{
   // Services
   private readonly paintPatternService = inject(PaintPatternService);
 
   // Signals
   readonly paintPattern: Signal<string[][]> = this.paintPatternService.paintPattern;
+
+  // For dynamic sizing
+  private readonly hostRef = inject(ElementRef);
+  private readonly resizeObserver = new ResizeObserver(() => this.resizeComponent());
+  readonly width = signal(0);
+  readonly height = signal(0);
+
+  ngAfterViewInit() {
+    this.resizeObserver.observe(this.hostRef.nativeElement);
+    this.resizeComponent();
+  }
+
+  ngOnDestroy() {
+    this.resizeObserver.unobserve(this.hostRef.nativeElement);
+  }
+
+  resizeComponent() {
+    const hostElement = this.hostRef.nativeElement as HTMLElement;
+    const { width: hostWidth, height: hostHeight } = hostElement.getBoundingClientRect();
+    this.width.set(Math.min(hostWidth, hostHeight));
+    this.height.set(Math.min(hostWidth, hostHeight));
+  }
 }
