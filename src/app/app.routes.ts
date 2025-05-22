ba284fbdd3336054dccc767e3b527b7ed2c24fbf
Kyle Reinholdtsen <kyle@reinholdtsen.com>
2025-05-22 08:54:39 -0700

    Create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,19 @@ export const routes: Routes = [
     loadComponent: () => {
       return import('./home/home.component').then((m) => m.HomeComponent);
     }
-},
-{
+  },
+  {
     path: 'about',
     pathMatch: 'full',
     loadComponent: () => {
       return import('./about/about.component').then((m) => m.AboutComponent);
+    }
+  },
+  {
+    path: 'auth/login',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import('./login/login.component').then((m) => m.LoginComponent);
     }
   }
 ];
